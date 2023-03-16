54567830921779c14cfeb6eff81e555b726b9a79
Robertblazor <96743273+Robertblazor@users.noreply.github.com>
2023-03-15 23:18:08 -0400

    March-15-2023

--- src/WebUi/Program.cs
+++ src/WebUi/Program.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using WebUi.Data;
+using Syncfusion.Blazor;
 
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTMxNjEzMkAzMjMwMmUzNDJlMzBDenJaUUxpOUNsWFZFV1VTQVRKMnBFTXltUWhJNFkwcnA0Qkw2c2wybDZVPQ==");
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddSyncfusionBlazor();
 
 var app = builder.Build();
 
